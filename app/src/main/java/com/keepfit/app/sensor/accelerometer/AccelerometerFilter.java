994a1bb2e26753936fca1a2329e768ff8911c351
cjsheehan <Chris.Sheehan@ymail.com>
2016-02-19 19:41:09 +0000

    Refactored (moved most non-app sepcific code to StepDetector library)

--- app/src/main/java/com/keepfit/app/sensor/accelerometer/AccelerometerFilter.java
+++
@@ -1,5 +0,0 @@
-package com.keepfit.app.sensor.accelerometer;
-
-public interface AccelerometerFilter {
-    float[] filter(float x, float y, float z);
-}
--- app/src/main/java/com/keepfit/app/sensor/accelerometer/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.keepfit.app.sensor.accelerometer;
-
-public class Constants {
-    public static final float HIGH_PASS_ALPHA = 0.8f;
-    public static final float LOW_PASS_ALPHA = 0.15f;
-    public static final int HIGH_PASS_MINIMUM = 10;
-    public static final int ACCELEROMETER_VECTOR_SIZE = 3;
-    public static final int SAMPLE_RATE = 10000; // (microsec -> sample rate = 100Hz)
-}
