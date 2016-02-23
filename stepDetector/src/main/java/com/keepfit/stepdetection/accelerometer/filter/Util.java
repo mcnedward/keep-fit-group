bc243663e5dfa18df5672e6e6098826f2b951825
Snooper00 <kornel.kotan@gmail.com>
2016-02-23 15:12:25 +0000

    floats changed to doubles

--- stepDetector/src/main/java/com/keepfit/stepdetection/accelerometer/filter/Util.java
+++ stepDetector/src/main/java/com/keepfit/stepdetection/accelerometer/filter/Util.java
@@ -2,20 +2,20 @@ package com.keepfit.stepdetection.accelerometer.filter;
 
 public class Util {
 
-    public static double magnitude(float[] values) {
+    public static double magnitude(double[] values) {
         if (values == null) throw new IllegalArgumentException("ERROR: values arg cannot be null");
         if (values.length < 1)
             throw new IllegalArgumentException("ERROR: values array arg cannot be empty");
         return Math.sqrt(sumOfSquares(values));
     }
 
-    public static float sumOfSquares(float[] values) {
+    public static float sumOfSquares(double[] values) {
         if (values == null) throw new IllegalArgumentException("ERROR: values arg cannot be null");
         if (values.length < 1)
             throw new IllegalArgumentException("ERROR: values array arg cannot be empty");
         float sos = 0;
         double squared = 2;
-        for (float f : values) {
+        for (double f : values) {
             sos += Math.pow(f, squared);
         }
         return sos;
--- stepDetector/src/main/java/com/keepfit/stepdetection/algorithms/AccelerationData.java
+++ stepDetector/src/main/java/com/keepfit/stepdetection/algorithms/AccelerationData.java
@@ -1,33 +1,39 @@
 package com.keepfit.stepdetection.algorithms;
 
+import com.keepfit.stepdetection.accelerometer.filter.Util;
+
 public class AccelerationData {
 
-    private final float x;
-    private final float y;
-    private final float z;
+    private final double x;
+    private final double y;
+    private final double z;
     private final long timeStamp;
 
     public AccelerationData(float x, float y, float z, long timeStamp) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+        this.x = (double) x;
+        this.y = (double)y;
+        this.z = (double)z;
         this.timeStamp = timeStamp;
     }
 
-    public float getX() {
+    public double getX() {
         return x;
     }
 
-    public float getY() {
+    public double getY() {
         return y;
     }
 
-    public float getZ() {
+    public double getZ() {
         return z;
     }
 
     public long getTimeStamp() {
         return timeStamp;
+    }
+
+    public double getXYZMagnitude(){
+        return Util.magnitude(new double[]{x, y, z});
     }
 
 }
